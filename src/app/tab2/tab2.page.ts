fb4c9033bb3377a20ee77df30606715d764edd21
Lucas 'Barba' Sousa da Silva <silvasousalucas@live.com>
2024-04-20 20:02:53 -0300

    terminando a calculadora pica

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { registerPlugin } from '@capacitor/core';
 
 @Component({
@@ -8,24 +8,95 @@ import { registerPlugin } from '@capacitor/core';
 })
 export class Tab2Page {
 
+  @ViewChild('ON3') obesidadeIII!: ElementRef;
+  @ViewChild('ON2') obesidadeII!: ElementRef;
+  @ViewChild('ON1') obesidadeI!: ElementRef;
+  @ViewChild('SPS') sobrepeso!: ElementRef;
+  @ViewChild('NML') normal!: ElementRef;
+  @ViewChild('PBX') pesoBaixo!: ElementRef;
+  @ViewChild('RES') resultadoDiv!: ElementRef;
+  @ViewChild('RESINPUT') resultadoInput!: ElementRef;
+
+  
+
+ 
   constructor() {}
 
-  public peso = 0;
-  public altura = 0;
-
+  public peso: number = 0;
+  public altura: number = 0;
+  public resultado: number = 0
   
   
-  public calculateIMC() {
+  private calcularIMC():number {
 
     const restult = this.peso / (this.altura*this.altura)
     return restult
   }
 
-  public mostrar(){
-    const result = this.calculateIMC()
+  public limparCampos():void{
+    this.altura = 0;
+    this.peso = 0;
+    this.resultado = 0;
 
-    
+    this.limparTabela()
+    this.mostrarResultado(this.resultado)
   }
+
+  private selecionarClassificacao(resultado: number):void{
+    if(resultado >= 40){
+      console.log("Você tá fudido, tua alma tá na mão do capeta")
+      this.obesidadeIII.nativeElement.classList.add("select")
+    }else if(resultado < 40 && resultado >= 35){
+      console.log("Você tá gordo pra caralho, teu corpo tá na mão capeta")
+      this.obesidadeII.nativeElement.classList.add("select")
+    }else if(resultado < 35 && resultado >= 30){
+      this.obesidadeI.nativeElement.classList.add("select")
+      console.log("Tá gordo, aproveita e come mais, tu tá dando o braço pro capeta")
+    }else if(resultado < 30 && resultado >= 25){
+      this.sobrepeso.nativeElement.classList.add("select")
+      console.log("Tá gordinho, você deu a mão para o capeta, continue assim.")
+    }else if(resultado < 25 && resultado >= 18.5){
+      this.normal.nativeElement.classList.add("select")      
+      console.log("Tá gordo não, tua alma tá na mão de Jesus")
+    }else if(resultado < 18.5){
+      console.log("Você é africano, Etíope? Tá cumendo barro?")
+      this.pesoBaixo.nativeElement.classList.add("select")
+    }
+
+  }
+
+  private mostrarResultado (resultado: number):void{
+    if(resultado > 0){
+      this.resultadoDiv.nativeElement.classList.remove("resultadoHidden")
+    }else{
+      this.resultadoDiv.nativeElement.classList.add("resultadoHidden")
+    }
+  }
+
+  private limparTabela(){
+    this.obesidadeIII.nativeElement.classList.remove("select")
+    this.obesidadeII.nativeElement.classList.remove("select")
+    this.obesidadeI.nativeElement.classList.remove("select")
+    this.sobrepeso.nativeElement.classList.remove("select")
+    this.normal.nativeElement.classList.remove("select")
+    this.pesoBaixo.nativeElement.classList.remove("select")
+  }
+
+  public initializer(){
+    let result = this.calcularIMC().toFixed(2)
+    this.resultado = Number(result)
+
+    this.mostrarResultado(this.resultado)
+    this.limparTabela()
+    this.selecionarClassificacao(this.resultado)
+    
+    
+       
+  }
+
+
+
+
 
 
 
